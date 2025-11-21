8ae5c6449d0dc6d095b00c67dd9f2cc585ecf2cf
jialeidng <jialei.ding@tudelft.nl>
2025-11-21 15:20:06 +0100

    added solution

--- max_even_square_solution.py
+++ max_even_square_solution.py
@@ -4,12 +4,28 @@ numbers = [3, 7, 2, 8, 5, 10, 6]
 def max_even_square(nums):
     max_num = 0
     for n in nums:
-        if n % 2 == 1: 
+        if n % 2 == 1:                  # checking for even numbers, not odd
             if n > max_num:
                 max_num = n
-    square = max_num * 2 
+    square = max_num * 2                # multiplied by 2, should be squared
     return square
 
 print("The max even number squared is:", max_even_square(numbers))
 
 max_even_square(numbers)
+
+
+# Correct version:
+numbers = [3, 7, 2, 8, 5, 10, 6]
+
+def max_even_square(nums):
+    max_num = None
+    for n in nums:
+        if n % 2 == 0:          
+            if max_num is None or n > max_num:
+                max_num = n
+
+    square = max_num ** 2      
+    return square
+
+print("The max even number squared is:", max_even_square(numbers))
